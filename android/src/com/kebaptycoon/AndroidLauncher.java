3a1daa6f8d2c1708acfdf10827e8cccee8605b6a
Dogancan Demirtas <dogancan.demirtas@ug.bilkent.edu.tr>
2016-02-12 02:10:21 +0200

    A clean LibGDX project is generated.

---
+++ android/src/com/kebaptycoon/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kebaptycoon;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kebaptycoon.KebapTycoonGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KebapTycoonGame(), config);
+	}
+}
