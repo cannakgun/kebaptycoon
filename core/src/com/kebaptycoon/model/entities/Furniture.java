739fbb55e2166bcf04480031ffd819358d5ed27f
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-03-02 14:09:59 +0200

    Implement Furniture.Type enum

--- core/src/com/kebaptycoon/model/entities/Furniture.java
+++ core/src/com/kebaptycoon/model/entities/Furniture.java
@@ -1,7 +1,13 @@
 package com.kebaptycoon.model.entities;
 
-/**
- * Created by dogancandemirtas on 27/02/16.
- */
 public class Furniture extends Entity{
+	
+	public static enum Type{
+		Cooker,
+		Oven,
+		Frier,
+		ServingTable,
+		Table,
+		Decoration
+	}
 }
