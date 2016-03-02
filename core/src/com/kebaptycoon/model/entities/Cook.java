6deaa1da3a23601eff59f2ad8974d4f694a028c3
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-03-02 15:13:49 +0200

    Implement Cook, Hawker and Waiter classes
    Cooks have a simple routine to them. Wait for an order to arrive, follow
    the process of the order and generate a dish for the recipe to the
    serving table.Likewise Waiters have a simple routine to them. Wait for
    a dish to be placed to the serving table, bring the dish to the customer
    who ordered it.
    
    Hawkers are a special type of employees that cannot be hired. They are
    only present at the starting of the game when the player only has a food
    stall. They are hybrid of Cooks and Waiters. They both prepare and
    deliver the dish to the customer. However one significant difference is
    that hawkers do not need to follow the process of the dish, they can
    make anything at their stall without using any explicit cookers, ovens,
    etc. It is implied that their stall has all the tools they need to make
    a dish.

--- core/src/com/kebaptycoon/model/entities/Cook.java
+++ core/src/com/kebaptycoon/model/entities/Cook.java
@@ -1,7 +1,42 @@
 package com.kebaptycoon.model.entities;
 
-/**
- * Created by dogancandemirtas on 27/02/16.
- */
-public class Cook extends Person{
+public class Cook extends Employee{
+	
+	public static enum State{
+		Wait,
+		PrepareOrder
+	}
+	
+	private Order currentOrder;
+	private State state;
+	
+	public Cook(int speed, String spriteName)
+	{
+		super(speed,spriteName);
+		this.currentOrder = null;
+		this.state = State.Wait;
+	}
+	
+	public Cook(int speed, String spriteName, int level)
+	{
+		super(speed,spriteName, level);
+		this.currentOrder = null;
+		this.state = State.Wait;
+	}
+
+	public Order getCurrentOrder() {
+		return currentOrder;
+	}
+
+	public void setCurrentOrder(Order currentOrder) {
+		this.currentOrder = currentOrder;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
 }
--- core/src/com/kebaptycoon/model/entities/Waiter.java
+++ core/src/com/kebaptycoon/model/entities/Waiter.java
@@ -1,7 +1,51 @@
 package com.kebaptycoon.model.entities;
 
-/**
- * Created by dogancandemirtas on 27/02/16.
- */
-public class Waiter extends Person{
+public class Waiter extends Employee{
+	
+	public static enum State{
+		Wait,
+		DeliverOrder
+	}
+	
+	private Order 	currentOrder;
+	private Dish 	currentDish;
+	private State 	state;
+	
+	public Waiter(int speed, String spriteName)
+	{
+		super(speed,spriteName);
+		this.currentOrder = null;
+		this.state = State.Wait;
+	}
+	
+	public Waiter(int speed, String spriteName, int level)
+	{
+		super(speed,spriteName, level);
+		this.currentOrder = null;
+		this.state = State.Wait;
+	}
+
+	public Order getCurrentOrder() {
+		return currentOrder;
+	}
+
+	public void setCurrentOrder(Order currentOrder) {
+		this.currentOrder = currentOrder;
+	}
+
+	public Dish getCurrentDish() {
+		return currentDish;
+	}
+
+	public void setCurrentDish(Dish currentDish) {
+		this.currentDish = currentDish;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
 }
