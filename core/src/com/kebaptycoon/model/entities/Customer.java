be990c3a2ac4bf0d517f353530a38795317d5834
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-03-02 13:59:08 +0200

    Implement Customer and CustomerPack classes
    A customer is any person on any of the venues that wishes to dine at our
    venue. Primary characteristics of a customer is that they have a type, a
    time period of waiting for their food and a budget. Individual waiting
    time and budget is usually determined by the type of the customer but in
    some cases it might be different (See customer packs below for details).
    
    A customer has a simple lifecycle. It first waits to be assigned to a
    table by the TableManager. After that it goes to its table and orders a
    food, influenced by the likes and dislikes of its type. If it waits too
    long for the food to be made it prematurely leaves, wasting time and
    resources for an unused food. Note that customer waits for the food to
    be made, not delivered, if a dish is placed to the serving table of the
    restaurant customer will not leave for waiting even if it takes two days
    to be delivered. After getting its food the customer spends some time to
    actually consume the dish and after eating it evaluates the food. Logic
    for evaluation of the dishes is not implemented here as it is one of the
    responsibilities of the game logic. After evaluation the customer waits
    for other members of the pack to finish and then simply pay and leave.
    
    CustomerPacks is a group of customers who wants to dine together at our
    restaurant. Primary differences in behaviour between singular customers
    and packs are they are seated together and the budget and the waiting
    time of the pack members are updated to the maximum value in the pack.
    Also if any of the pack member's dish gets made then entire pack will
    wait their foods. These properties along with customers waiting other
    pack memebers to pay emulate both the union of pack, by sychronized
    waiting and leaving conditions, and the fact that groups of customers
    are likely to spend more money per person.

--- core/src/com/kebaptycoon/model/entities/Customer.java
+++ core/src/com/kebaptycoon/model/entities/Customer.java
@@ -1,7 +1,103 @@
 package com.kebaptycoon.model.entities;
 
-/**
- * Created by dogancandemirtas on 27/02/16.
- */
+import java.util.ArrayList;
+
 public class Customer extends Person{
+
+	public enum Type {
+		;//TODO: Create customer archetypes
+
+		private int 				waitingTime;
+		private int 				budget;
+		private ArrayList<Recipe> 	likes;
+		private ArrayList<Recipe> 	dislikes;
+		
+		private Type(int waitingTime, int budget, ArrayList<Recipe> likes, ArrayList<Recipe> dislikes) {
+			this.waitingTime = waitingTime;
+			this.budget = budget;
+			this.likes = likes;
+			this.dislikes = dislikes;
+		}
+
+		public int getWaitingTime() {
+			return waitingTime;
+		}
+
+		public int getBudget() {
+			return budget;
+		}
+
+		public ArrayList<Recipe> getLikes() {
+			return likes;
+		}
+
+		public ArrayList<Recipe> getDislikes() {
+			return dislikes;
+		}
+	}
+	
+	public enum State {
+		WaitForTable,
+		GoToTable,
+		Order,
+		WaitForFood,
+		EatFood,
+		EvaluateFood,
+		WaitPack,
+		Pay,
+		Leave
+	}
+	
+	private Type type;
+	private int waitingTime;
+	private int budget;
+	private Dish dish;
+	private boolean waitOverride;
+	private State state;
+	
+	public Customer(int speed, String spriteName, Type type, int waitingTime, int budget) {
+		super(speed,spriteName);
+		this.type = type;
+		this.waitingTime = waitingTime;
+		this.budget = budget;
+		this.dish = null;
+		this.waitOverride = false;
+		this.state = State.WaitForTable;
+	}
+
+	public Dish getDish() {
+		return dish;
+	}
+
+	public void setDish(Dish dish) {
+		this.dish = dish;
+	}
+
+	public boolean isWaitOverride() {
+		return waitOverride;
+	}
+
+	public void setWaitOverride(boolean waitOverride) {
+		this.waitOverride = waitOverride;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public int getWaitingTime() {
+		return waitingTime;
+	}
+
+	public int getBudget() {
+		return budget;
+	}
 }
--- core/src/com/kebaptycoon/model/entities/CustomerPack.java
+++ core/src/com/kebaptycoon/model/entities/CustomerPack.java
@@ -1,7 +1,24 @@
 package com.kebaptycoon.model.entities;
 
-/**
- * Created by dogancandemirtas on 27/02/16.
- */
+import java.util.ArrayList;
+
 public class CustomerPack {
+	
+	private ArrayList<Customer> customers;
+
+	public CustomerPack() {
+		this.customers = new ArrayList<Customer>();
+	}
+
+	public CustomerPack(ArrayList<Customer> customers) {
+		this.customers = customers;
+	}
+
+	public ArrayList<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(ArrayList<Customer> customers) {
+		this.customers = customers;
+	}
 }
