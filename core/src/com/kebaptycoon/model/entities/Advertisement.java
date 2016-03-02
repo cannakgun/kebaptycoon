48c61bdfb6f92e3fc5d6fd9908614154c2b957a4
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-03-02 14:06:40 +0200

    Implement Advertisement class
    Advertisement objects are where we store the advertisements given by the
    player. They modify the popularity of certain demographics based on its
    properties An advertisement has 4 primary properties. Quality represent
    how much money is spent making the advert itself. Better quality means
    the more effective and rememberable the advert is. Duration represent
    how many days the advert will stay on the world. Platform means on which
    platform the advert is. Better platforms reach to more people. Focus
    represents the focused demographic of the advert. If you give an advert
    focused on teens you would see most of the result in teens.

--- core/src/com/kebaptycoon/model/entities/Advertisement.java
+++ core/src/com/kebaptycoon/model/entities/Advertisement.java
@@ -1,7 +1,92 @@
 package com.kebaptycoon.model.entities;
 
-/**
- * Created by dogancandemirtas on 27/02/16.
- */
+import java.util.Date;
+
+import com.kebaptycoon.model.entities.Customer.Type;
+
 public class Advertisement {
+	
+	public static enum Quality {
+		Low(1),
+		Medium(2),
+		High(3);
+
+		private int value;    
+		
+		private Quality(int value) {
+			this.value = value;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+	}
+	
+	public static enum Duration {
+		Day(1),
+		Week(7),
+		Month(30);
+
+		private int value;    
+		
+		private Duration(int value) {
+			this.value = value;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+	}
+	
+	public static enum Platform {
+		Newspaper(1),
+		Internet(2),
+		Billboard(3),
+		Television(5);
+
+		private int value;    
+		
+		private Platform(int value) {
+			this.value = value;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+	}
+	
+	private Quality quality;
+	private Duration duration;
+	private Platform platform;
+	private Customer.Type focus;
+	private Date startDate;
+	
+	public Advertisement(Quality quality, Duration duration, Platform platform, Type focus, Date startDate) {
+		this.quality = quality;
+		this.duration = duration;
+		this.platform = platform;
+		this.focus = focus;
+		this.startDate = startDate;
+	}
+
+	public Quality getQuality() {
+		return quality;
+	}
+
+	public Duration getDuration() {
+		return duration;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public Customer.Type getFocus() {
+		return focus;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+	
 }
