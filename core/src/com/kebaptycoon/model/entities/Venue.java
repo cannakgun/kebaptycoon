b70282de9e2090180033f3863888d312d3526134
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-03-02 15:19:52 +0200

    Implement Venue class
    Currently the only thing venue does is to be a container for the objects
    within it and store the properties of itself.

--- core/src/com/kebaptycoon/model/entities/Venue.java
+++ core/src/com/kebaptycoon/model/entities/Venue.java
@@ -1,7 +1,81 @@
 package com.kebaptycoon.model.entities;
 
-/**
- * Created by dogancandemirtas on 27/02/16.
- */
+import java.util.ArrayList;
+
 public class Venue {
+
+	private ArrayList<IngredientTuple> 	stock;
+	private ArrayList<Employee> 		employees;
+	private ArrayList<Furniture> 		furnitures;
+	private int							width;
+	private int							height;
+	private int							kitchenWidth;
+	private int							kitchenHeight;
+	private boolean						managed;
+	private boolean						operational;
+	
+	public Venue(int width, int height, int kitchenWidth, int kitchenHeight, boolean managed) {
+		this.width = width;
+		this.height = height;
+		this.kitchenWidth = kitchenWidth;
+		this.kitchenHeight = kitchenHeight;
+		this.managed = managed;
+	}
+
+	public ArrayList<IngredientTuple> getStock() {
+		return stock;
+	}
+
+	public void setStock(ArrayList<IngredientTuple> stock) {
+		this.stock = stock;
+	}
+
+	public ArrayList<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(ArrayList<Employee> employees) {
+		this.employees = employees;
+	}
+
+	public ArrayList<Furniture> getFurnitures() {
+		return furnitures;
+	}
+
+	public void setFurnitures(ArrayList<Furniture> furnitures) {
+		this.furnitures = furnitures;
+	}
+
+	public boolean isManaged() {
+		return managed;
+	}
+
+	public void setManaged(boolean managed) {
+		this.managed = managed;
+	}
+
+	public boolean isOperational() {
+		return operational;
+	}
+
+	public void setOperational(boolean operational) {
+		this.operational = operational;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getKitchenWidth() {
+		return kitchenWidth;
+	}
+
+	public int getKitchenHeight() {
+		return kitchenHeight;
+	}
+	
 }
