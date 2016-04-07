3e120a165cb6194a18d8ccd0c34ef6093b6d28cc
Dogancan Demirtas <dogancan.demirtas@ug.bilkent.edu.tr>
2016-04-07 20:06:42 +0300

    Refactor in Android Facebook login flow.
    LoginManager is used instead of FacebookLoginButton. Facebook user id is retrieved with GraphRequest.

--- android/src/com/kebaptycoon/AndroidFacebookActivity.java
+++ android/src/com/kebaptycoon/AndroidFacebookActivity.java
@@ -2,88 +2,81 @@ package com.kebaptycoon;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.Arrays;
+import java.util.List;
 
-/**
- * Created by Dogancan on 18/12/15.
- */
-public class AndroidFacebookActivity extends Activity{
-    private CallbackManager callbackManager;
-    private LoginButton loginButton;
+public class AndroidFacebookActivity extends Activity {
+
+    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        /*try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.kebaptycoon",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {}*/
-
         FacebookSdk.sdkInitialize(getApplicationContext());
+
         callbackManager = CallbackManager.Factory.create();
-        setContentView(R.layout.layout_android_facebook_activity);
-        final Preferences prefs = Gdx.app.getPreferences("Kebap Tycoon Preferences");
 
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-        Gdx.app.log("prefs facebook access token", prefs.getString("facebook_access_token"));
+        List<String> permissionNeeds= Arrays.asList("public_profile");
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        LoginManager.getInstance().logInWithReadPermissions(this, permissionNeeds);
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
+            public void onSuccess(LoginResult loginResults) {
+                String token = loginResults.getAccessToken().getToken();
+                KebapTycoonGame.getInstance().getPrefs().putString("facebook_access_token", token);
 
-                prefs.putString("facebook_access_token", loginResult.getAccessToken().getToken());
-                prefs.flush();
-                System.out.println("token saved: " + prefs.getString("facebook_access_token"));
-                Toast.makeText(getApplicationContext(), prefs.getString("facebook_access_token"), Toast.LENGTH_LONG).show();
+                GraphRequest request = GraphRequest.newMeRequest(loginResults.getAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
 
-                finish();
+                        try {
+                            KebapTycoonGame.getInstance().getPrefs().putString("facebook_user_id", object.getString("id"));
+                            KebapTycoonGame.getInstance().getPrefs().flush();
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id");
+                request.setParameters(parameters);
+                request.executeAsync();
             }
-
             @Override
             public void onCancel() {
+                Log.e("AndroidFacebookActivity", "Facebook login cancelled.");
+                finish();
             }
-
             @Override
             public void onError(FacebookException e) {
-                Toast.makeText(getApplicationContext(), "Facebook error occured. Please try again later.",
-                        Toast.LENGTH_LONG).show();
-                Gdx.app.log("ERROR: ", e.getMessage());
+                Log.e("AndroidFacebookActivity", "Facebook login error.");
                 finish();
             }
         });
-
-        loginButton.performClick();
-
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
