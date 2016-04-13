15254f7168a704ce5bca4e370525ef1801941ed9
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-04-13 16:42:29 +0300

    Add animation capabilities to entity

--- core/src/com/kebaptycoon/model/entities/Entity.java
+++ core/src/com/kebaptycoon/model/entities/Entity.java
@@ -1,23 +1,40 @@
 package com.kebaptycoon.model.entities;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class Entity {
 	private int x;
 	private int y;
 	private int z;
+    private Animation animation;
+    private float animationTime;
 	
 	public Entity() {
 		this.x = 0;
 		this.y = 0;
 		this.z = 0;
+        animation = null;
+        animationTime = 0f;
 	}
 	
 	public Entity(int x, int y, int z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+        animation = null;
+        animationTime = 0f;
 	}
 
-	public int getX() {
+    public Entity(int x, int y, int z, Animation animation) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.animation = animation;
+        animationTime = 0;
+    }
+
+    public int getX() {
 		return x;
 	}
 
@@ -40,4 +57,25 @@ public class Entity {
 	public void setZ(int z) {
 		this.z = z;
 	}
+
+    public Animation getAnimation() {
+        return animation;
+    }
+
+    public TextureRegion cycleAnimation(float delta) {
+        incrementAnimationTime(delta);
+        return getCurrentFrame();
+    }
+
+    public TextureRegion getCurrentFrame() {
+        return this.animation.getKeyFrame(animationTime);
+    }
+
+    public void incrementAnimationTime(float delta) {
+        this.animationTime += delta;
+    }
+
+    public void resetAnimationTime() {
+        this.animationTime = 0f;
+    }
 }
