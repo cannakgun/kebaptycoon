04d60649efec5e0cdb93430147e50b84f3a4969b
Can Akgün <canakgun@outlook.com>
2016-04-16 16:47:05 +0300

    Implement Furniture and Orientation enum

--- core/src/com/kebaptycoon/model/entities/Furniture.java
+++ core/src/com/kebaptycoon/model/entities/Furniture.java
@@ -8,6 +8,54 @@ public class Furniture extends Entity{
 		Frier,
 		ServingTable,
 		Table,
-		Decoration
+		Decoration,
+		Grill
 	}
+    String description;
+    String name;
+    Type type;
+    Orientation orientation;
+    int width;
+    int height;
+    Person user;
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Person getUser() {
+        return user;
+    }
+
+    public void setUser(Person user) {
+        this.user = user;
+    }
 }
---
+++ core/src/com/kebaptycoon/model/entities/Orientation.java
@@ -0,0 +1,11 @@
+package com.kebaptycoon.model.entities;
+
+/**
+ * Created by Can Akgün on 16.4.2016.
+ */
+public enum Orientation {
+    East,
+    North,
+    West,
+    South
+}
