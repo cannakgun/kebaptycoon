4331cb9b742fcc188cbb5fe055507dfe6d51f318
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-04-16 20:49:16 +0300

    Create skeleton game logic

--- core/src/com/kebaptycoon/model/logic/GameLogic.java
+++ core/src/com/kebaptycoon/model/logic/GameLogic.java
@@ -1,18 +1,56 @@
 package com.kebaptycoon.model.logic;
 
+import com.kebaptycoon.model.entities.*;
 import com.kebaptycoon.model.managers.*;
 
-import java.util.Date;
+import java.util.Calendar;
 
 public class GameLogic {
 
     private RecipeManager recipeManager;
-    private Date date;
+    private AdvertisementManager advertisementManager;
+    private CustomerManager customerManager;
+    private MarketManager marketManager;
+    private OrderManager orderManager;
+    private VenueManager venueManager;
+    private Calendar date;
     private int money;
 
     public GameLogic(){
 
         recipeManager = new RecipeManager();
+        advertisementManager = new AdvertisementManager();
+        customerManager = new CustomerManager();
+        marketManager = new MarketManager();
+        orderManager = new OrderManager();
+        venueManager = new VenueManager();
+        date = new Calendar.Builder().setTimeOfDay(6, 0, 0).build();
+    }
+
+    public void update() {
+        if(date.get(Calendar.HOUR) >= 18) {
+            //After hours
+        }
+        else {
+            //Working hours
+            date.add(Calendar.MINUTE, 1);
+
+            for (Venue venue: venueManager.getVenueList()) {
+
+                //Execute person AI
+                for (Customer customer: venue.getCustomers()) {
+                    customer.think();
+                }
+
+                for (Employee employee: venue.getEmployees()) {
+                    employee.think();
+                }
+            }
+
+            //Customer generation
+
+
+        }
 
     }
 
@@ -20,9 +58,33 @@ public class GameLogic {
         return recipeManager;
     }
 
+    public AdvertisementManager getAdvertisementManager() {
+        return advertisementManager;
+    }
+
+    public CustomerManager getCustomerManager() {
+        return customerManager;
+    }
+
+    public MarketManager getMarketManager() {
+        return marketManager;
+    }
+
+    public OrderManager getOrderManager() {
+        return orderManager;
+    }
+
+    public VenueManager getVenueManager() {
+        return venueManager;
+    }
+
     public int getMoney() {
 
         return money;
+    }
+
+    public Calendar getDate() {
+        return date;
     }
 
     public void setMoney(int money) {
