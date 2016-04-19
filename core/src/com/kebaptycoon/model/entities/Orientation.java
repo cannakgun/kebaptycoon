ed6ba442397ecfbd4c5abb58dac2d5990e0d8379
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-04-19 12:21:31 +0300

    Add couple methods to Orientation

--- core/src/com/kebaptycoon/model/entities/Orientation.java
+++ core/src/com/kebaptycoon/model/entities/Orientation.java
@@ -1,5 +1,7 @@
 package com.kebaptycoon.model.entities;
 
+import com.badlogic.gdx.math.Vector3;
+
 /**
  * Created by Can Akgün on 16.4.2016.
  */
@@ -7,5 +9,33 @@ public enum Orientation {
     East,
     North,
     West,
-    South
+    South;
+
+    public Vector3 getUnitVector() {
+        switch (this) {
+            case East:
+                return new Vector3(1,0,0);
+            case North:
+                return new Vector3(0,1,0);
+            case West:
+                return new Vector3(-1,0,0);
+            case South:
+                return new Vector3(0,-1,0);
+        }
+        return null;
+    }
+
+    public Orientation getReverse() {
+        switch (this) {
+            case East:
+                return West;
+            case North:
+                return South;
+            case West:
+                return East;
+            case South:
+                return North;
+        }
+        return null;
+    }
 }
