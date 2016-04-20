8a12f15062cafcd9fc3ea3af13954e7d5508a57d
Ulaş Sophylax Sert <sophylax@sophylax.net>
2016-04-20 14:04:27 +0300

    Create prototype classes for Furniture and Venue

---
+++ core/src/com/kebaptycoon/model/entities/prototypes/VenuePrototype.java
@@ -0,0 +1,14 @@
+package com.kebaptycoon.model.entities.prototypes;
+
+public class VenuePrototype {
+
+    public int height;
+    public int kitchenWidth;
+    public int kitchenHeight;
+    public boolean operational;
+    public float spawnX;
+    public float spawnY;
+    public String background;
+
+    public VenuePrototype() {}
+}
